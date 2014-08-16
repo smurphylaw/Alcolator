c8649675978cb2a28011621a7a9c978033b727c8
Stephen Murphy <smurphylaw@gmail.com>
2014-08-16 15:24:56 -0600

    Initial Commit

---
+++ Alcolator/BLCViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCViewController.h
+//  Alcolator
+//
+//  Created by Murphy on 8/16/14.
+//  Copyright (c) 2014 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCViewController : UIViewController
+
+@end
---
+++ Alcolator/BLCViewController.m
@@ -0,0 +1,29 @@
+//
+//  BLCViewController.m
+//  Alcolator
+//
+//  Created by Murphy on 8/16/14.
+//  Copyright (c) 2014 Murphy. All rights reserved.
+//
+
+#import "BLCViewController.h"
+
+@interface BLCViewController ()
+
+@end
+
+@implementation BLCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
